3f3fddf7f038e5979f52774d61c9f768d8a7f449
keenmaster486 <cebrock106@gmail.com>
2019-05-07 10:55:50 -0600

    Added a geo thing

---
+++ src/Map/Map.jsx
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+class Map extends Component
+{
+	constructor()
+	{
+		super()
+	}
+
+	render()
+	{
+		return(
+			<div>
+				<h1>HERE IS THE MAP COMPONENT</h1>
+			</div>
+		);
+	}
+}
+
+export default Map;
